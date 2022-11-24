9dfa9067d4f5711360d2d818030efa7eee0f84c7
kev-put <118623150+kev-put@users.noreply.github.com>
2022-11-23 21:37:04 -0500

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+
+  let k=100
+function setup() {
+  createCanvas(windowWidth, windowHeight,WEBGL);
+
+}
+
+function draw() {
+  background(220,100,0);
+  torus (k,60,)
+  fill(100,20,100)
+  k++
+}
