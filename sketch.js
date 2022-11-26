f63bd24fe2269031bbc2b92873168708b4e7a992
kev-put <118623150+kev-put@users.noreply.github.com>
2022-11-26 09:27:04 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -7,30 +7,35 @@ let randoSix
 let randoSeven
 let randoEight
 let randoNine
+let randoTen
+let x
 
 function setup() {
   createCanvas(windowWidth,windowHeight);
   background(0);
   
+  
 }
 
 function draw() {
   
+  
 
-  randoOne = random(10,20)
-  randoTwo = random(10,20)
+  randoOne = random(5,10)
+  randoTwo = random(5,10)
 randoThree = random (200)
   randoFour = random(80,200)
   randoFive = random(width)
   randoSix = random(height)
-  randoSeven = random(20,80)
+  randoSeven = random(20,60)
   randoEight = random(10,90)
   randoNine = random(10,30)
-  fill(randoOne,randoTwo,randoThree,randoFour);
-  ellipse (randoFive,randoSix, randoOne,randoTwo);
+  x = random(5,10)
+   fill(100,randoTwo,randoThree,randoFour)
+  ellipse (randoFive,randoSix, randoOne,randoTwo)
   
-  strokeWeight(randoNine);
-  stroke(randoThree,randoFour,randoTwo,randoSeven);
+  strokeWeight(randoNine)
+  stroke(randoThree,randoFour,randoTwo,randoSeven)
   
   
 
