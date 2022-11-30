9e82ce5ab2a918e5b4fe46473430856387168756
kev-put <118623150+kev-put@users.noreply.github.com>
2022-11-30 18:26:02 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,10 +1,10 @@
 let x, y, a, b;
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  x = 1;
-  y = 1;
-  a = 1;
-  b = 1;
+  x = 2;
+  y = 2;
+  a = 2;
+  b = 2;
   background(0);
   fill(100,0,0)
 }
